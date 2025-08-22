8a843cf2c540abe799a57f7b61753b0031f12040
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2025-08-22 22:12:49 +1000

    Added shooting and splitting asteroids

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
     
 
 def main():
@@ -20,8 +21,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
@@ -43,6 +46,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         # Render Screen and sprites
         screen.fill("black")
