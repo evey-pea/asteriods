068a26f67a1d67f6dfeea02a124e882407846b78
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2025-08-22 20:34:03 +1000

    Added 60 FPS cap

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, "black")
-        pygame.display.flip() 
-
+        pygame.display.flip()
+        
+        # FPS Limit 60
+        dt = (clock.tick(60))/1000
 
 if __name__ == "__main__":
     main()
